8bc1532a5c78fb4f8621f80b77324b7df543ff72
Chris O'Mara <comara17@gmail.com>
2017-06-27 15:14:27 -0400

    Added javascript link

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
+$(document).on('click', '#search', function() {
+	window.location = 'grid.html';
+});
+
+//Initialize Facebook API
 window.fbAsyncInit = function() {
     FB.init({
       appId      : '679455808909655',
@@ -7,6 +12,7 @@ window.fbAsyncInit = function() {
     });
     FB.AppEvents.logPageView();   
   };
+  
  (function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
@@ -15,6 +21,7 @@ window.fbAsyncInit = function() {
      fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
+//Creates a Facebook login button
 (function(d, s, id) {
 	  var js, fjs = d.getElementsByTagName(s)[0];
 	  if (d.getElementById(id)) return;
@@ -23,19 +30,34 @@ window.fbAsyncInit = function() {
 	  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
  
+ //If Faceback callback for login status is connected, redirect to main html page
  var checkLogin = function() {
  	document.location.href = 'grid.html';
  	}
 	
+//Initialize GoogleMaps API	
  function initMap() {
-	var uluru = {lat: 28.54, lng: -81.38};
+	var florida = {lat: 28.54, lng: -81.38};
 	var map = new google.maps.Map(document.getElementById('map'), {
-	zoom: 4,
-	center: uluru
+	zoom: 6,
+	center: florida
 	});
 	var marker = new google.maps.Marker({
-	position: uluru,
+	position: florida,
 	map: map
 	});
 }
- 
+
+//Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBRk12SsqUScaiXgY2oUgVRIhHRpKqzPq0",
+    authDomain: "yoink-1b31d.firebaseapp.com",
+    databaseURL: "https://yoink-1b31d.firebaseio.com",
+    projectId: "yoink-1b31d",
+    storageBucket: "yoink-1b31d.appspot.com",
+    messagingSenderId: "426906359481"
+  };
+  firebase.initializeApp(config);
+  
+  
+  
