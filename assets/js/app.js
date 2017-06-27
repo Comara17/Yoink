43598b690f50fe4e0b0ae9b12feb592a039819ba
Chris O'Mara <comara17@gmail.com>
2017-06-27 16:28:44 -0400

    Fixed a line issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,70 +1,93 @@
+$(document).ready(function() {
 
-//Initialize Facebook API
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '679455808909655',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();   
-  };
-  
- (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+	//Initialize Facebook API
+	window.fbAsyncInit = function() {
+		FB.init({
+		  appId      : '679455808909655',
+		  cookie     : true,
+		  xfbml      : true,
+		  version    : 'v2.8'
+		});
+		FB.AppEvents.logPageView();   
+	  };
 
-//Creates a Facebook login button
-(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=1679455808909655";
-	  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
- 
- //If Faceback callback for login status is connected, redirect to main html page
- var checkLogin = function() {
- 	document.location.href = 'grid.html';
- 	}
+	 (function(d, s, id){
+		 var js, fjs = d.getElementsByTagName(s)[0];
+		 if (d.getElementById(id)) {return;}
+		 js = d.createElement(s); js.id = id;
+		 js.src = "//connect.facebook.net/en_US/sdk.js";
+		 fjs.parentNode.insertBefore(js, fjs);
+	  }(document, 'script', 'facebook-jssdk'));
 
-FB.getLoginStatus(function(response) {
-  var loginButton = document.getElementById('navLoginBtn');
-  var postButton = document.getElementById('navPostBtn')
-  if (response.status === 'connected') {
-    loginButton.style.display = 'none';
-	} else {
-	postButton.style.display = 'none';
+	//Creates a Facebook login button
+	(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=1679455808909655";
+		  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+	 //If Faceback callback for login status is connected, redirect to main html page
+	 var checkLogin = function() {
+		document.location.href = 'grid.html';
+		}
+
+//
+//	 FB.getLoginStatus(function(response) {
+//	  if (response.status === 'connected') {
+//		// the user is logged in and has authenticated your
+//		// app, and response.authResponse supplies
+//		// the user's ID, a valid access token, a signed
+//		// request, and the time the access token 
+//		// and signed request each expire
+//		var uid = response.authResponse.userID;
+//		var accessToken = response.authResponse.accessToken;
+//	  } else if (response.status === 'not_authorized') {
+//		// the user is logged in to Facebook, 
+//		// but has not authenticated your app
+//	  } else {
+//		// the user isn't logged in to Facebook.
+//	  }
+//	 });
+
+
+
+	FB.getLoginStatus(function(response) {
+	  var loginButton = document.getElementById('navLoginBtn');
+	  var postButton = document.getElementById('navPostBtn');
+	  if (response.status === 'connected') {
+		loginButton.style.display = 'none';
+		} else {
+		postButton.style.display = 'none';
+		}
+	});
+
+	//Initialize GoogleMaps API	
+	 function initMap() {
+		var florida = {lat: 28.54, lng: -81.38};
+		var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 6,
+		center: florida
+		});
+		var marker = new google.maps.Marker({
+		position: florida,
+		map: map
+		});
 	}
-});
-	
-//Initialize GoogleMaps API	
- function initMap() {
-	var florida = {lat: 28.54, lng: -81.38};
-	var map = new google.maps.Map(document.getElementById('map'), {
-	zoom: 6,
-	center: florida
-	});
-	var marker = new google.maps.Marker({
-	position: florida,
-	map: map
-	});
-}
 
-//Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBRk12SsqUScaiXgY2oUgVRIhHRpKqzPq0",
-    authDomain: "yoink-1b31d.firebaseapp.com",
-    databaseURL: "https://yoink-1b31d.firebaseio.com",
-    projectId: "yoink-1b31d",
-    storageBucket: "yoink-1b31d.appspot.com",
-    messagingSenderId: "426906359481"
-  };
-  firebase.initializeApp(config);
+	//Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyBRk12SsqUScaiXgY2oUgVRIhHRpKqzPq0",
+		authDomain: "yoink-1b31d.firebaseapp.com",
+		databaseURL: "https://yoink-1b31d.firebaseio.com",
+		projectId: "yoink-1b31d",
+		storageBucket: "yoink-1b31d.appspot.com",
+		messagingSenderId: "426906359481"
+	  };
+	  firebase.initializeApp(config);
+
+});
   
 //End of initialization
 
